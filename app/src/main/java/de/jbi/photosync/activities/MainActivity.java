0981b501b0b7aedea15ecc74d2c3f14dfe3c48e7
Jan Bissinger <jan.bissinger@outlook.de>
2016-05-14 14:57:57 +0200

    Empty fragment implementation -> Use this for new projects
    - Navigation drawer
    - Toolbar
    - Empty fragment

--- app/src/main/java/de/jbi/photosync/activities/MainActivity.java
+++ app/src/main/java/de/jbi/photosync/activities/MainActivity.java
@@ -1,67 +1,109 @@
 package de.jbi.photosync.activities;
 
-import android.app.Activity;
-import android.content.Intent;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import de.jbi.photosync.R;
-//import android.app.Fragment;
-//import android.app.FragmentManager;
-//import android.content.res.Configuration;
-//import android.os.Bundle;
-//import android.support.v4.widget.DrawerLayout;
-//import android.support.v7.app.ActionBarDrawerToggle;
-//import android.view.LayoutInflater;
-//import android.view.MenuItem;
-//import android.view.View;
-//import android.view.ViewGroup;
-//import android.widget.AdapterView;
-//import android.widget.ArrayAdapter;
-//import android.widget.ImageView;
-//import android.widget.ListView;
-//
-//import java.util.Locale;
-//
-//import de.jbi.photosync.R;
+import de.jbi.photosync.fragments.EmptyFragment;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
+    private ListView drawerList;
+
+    private CharSequence title;
+    private String[] fragmentTitles;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent startInfoActivityIntent = new Intent(this, DeviceInfoActivity.class);
-        startActivity(startInfoActivityIntent);
+        fragmentTitles = getResources().getStringArray(R.array.fragments_array);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawerList = (ListView) findViewById(R.id.left_drawer);
+
+        drawerList.setAdapter(new ArrayAdapter<>(this, R.layout.drawer_list_item, fragmentTitles));
+        drawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
+
+        if (savedInstanceState == null) {
+            selectItem(0);
+        }
+    }
+
+    /* The click listner for ListView in the navigation drawer */
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+
+    private void selectItem(int position) {
+        // update the main content by replacing fragments
+        Fragment fragment = new EmptyFragment();
+        Bundle args = new Bundle();
+        args.putInt(EmptyFragment.ARG_FRAGMENT_NUMBER, position);
+        fragment.setArguments(args);
+
+        // Insert the fragment by replacing any existing fragment
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .commit();
+
+        // Highlight the selected item, update the title, and close the drawer
+        drawerList.setItemChecked(position, true);
+        setTitle(fragmentTitles[position]);
+        drawerLayout.closeDrawer(drawerList);
+    }
+
+    @Override
+    public void setTitle(CharSequence title) {
+        this.title = title;
+        getSupportActionBar().setTitle(this.title);
     }
 }
 
-// TODO: Handle: drawer_list_item.xml, fragment_planet.xml, strings entries
+
+// TODO: Handle: drawer_list_item.xml, fragment_emptyxml, strings entries
 
 
-//    private DrawerLayout mDrawerLayout;
-//    private ListView mDrawerList;
-//    private ActionBarDrawerToggle mDrawerToggle;
+//    private DrawerLayout drawerLayout;
+//    private ListView drawerList;
+//    private ActionBarDrawerToggle drawerToggle;
 //
-//    private CharSequence mDrawerTitle;
-//    private CharSequence mTitle;
-//    private String[] mPlanetTitles;
+//    private CharSequence drawerTitle;
+//    private CharSequence title;
+//    private String[] fragmentTitles;
 //
 //    @Override
 //    protected void onCreate(Bundle savedInstanceState) {
 //        super.onCreate(savedInstanceState);
 //        setContentView(R.layout.activity_main);
 //
-//        mTitle = mDrawerTitle = getTitle();
-//        mPlanetTitles = getResources().getStringArray(R.array.planets_array);
-//        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-//        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+//        title = drawerTitle = getTitle();
+//        fragmentTitles = getResources().getStringArray(R.array.planets_array);
+//        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+//        drawerList = (ListView) findViewById(R.id.left_drawer);
 //
 //        // set a custom shadow that overlays the main content when the drawer opens
-////        mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+////        drawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
 //        // set up the drawer's list view with items and click listener
-//        mDrawerList.setAdapter(new ArrayAdapter<>(this,
-//                R.layout.drawer_list_item, mPlanetTitles));
-//        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+//        drawerList.setAdapter(new ArrayAdapter<>(this,
+//                R.layout.drawer_list_item, fragmentTitles));
+//        drawerList.setOnItemClickListener(new DrawerItemClickListener());
 //
 //        // enable ActionBar app icon to behave as action to toggle nav drawer
 ////        getActionBar().setDisplayHomeAsUpEnabled(true);
@@ -69,24 +111,24 @@ public class MainActivity extends Activity {
 //
 //        // ActionBarDrawerToggle ties together the the proper interactions
 //        // between the sliding drawer and the action bar app icon
-//        mDrawerToggle = new ActionBarDrawerToggle(
+//        drawerToggle = new ActionBarDrawerToggle(
 //                this,                  /* host Activity */
-//                mDrawerLayout,         /* DrawerLayout object */
+//                drawerLayout,         /* DrawerLayout object */
 ////                R.drawable.ic_drawer,  /* nav drawer image to replace 'Up' caret */
 //                R.string.drawer_open,  /* "open drawer" description for accessibility */
 //                R.string.drawer_close  /* "close drawer" description for accessibility */
 //        ) {
 //            public void onDrawerClosed(View view) {
-//                getActionBar().setTitle(mTitle);
+//                getActionBar().setTitle(title);
 //                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
 //            }
 //
 //            public void onDrawerOpened(View drawerView) {
-//                getActionBar().setTitle(mDrawerTitle);
+//                getActionBar().setTitle(drawerTitle);
 //                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
 //            }
 //        };
-//        mDrawerLayout.setDrawerListener(mDrawerToggle);
+//        drawerLayout.setDrawerListener(drawerToggle);
 //
 //        if (savedInstanceState == null) {
 //            selectItem(0);
@@ -104,7 +146,7 @@ public class MainActivity extends Activity {
 ////    @Override
 ////    public boolean onPrepareOptionsMenu(Menu menu) {
 ////        // If the nav drawer is open, hide action items related to the content view
-////        boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
+////        boolean drawerOpen = drawerLayout.isDrawerOpen(drawerList);
 ////        menu.findItem(R.id.action_websearch).setVisible(!drawerOpen);
 ////        return super.onPrepareOptionsMenu(menu);
 ////    }
@@ -113,7 +155,7 @@ public class MainActivity extends Activity {
 //    public boolean onOptionsItemSelected(MenuItem item) {
 //        // The action bar home/up action should open or close the drawer.
 //        // ActionBarDrawerToggle will take care of this.
-//        if (mDrawerToggle.onOptionsItemSelected(item)) {
+//        if (drawerToggle.onOptionsItemSelected(item)) {
 //            return true;
 //        }
 //        // Handle action buttons
@@ -144,24 +186,24 @@ public class MainActivity extends Activity {
 //
 //    private void selectItem(int position) {
 //        // update the main content by replacing fragments
-//        Fragment fragment = new PlanetFragment();
+//        Fragment fragment = new EmptyFragment();
 //        Bundle args = new Bundle();
-//        args.putInt(PlanetFragment.ARG_PLANET_NUMBER, position);
+//        args.putInt(EmptyFragment.ARG_FRAGMENT_NUMBER, position);
 //        fragment.setArguments(args);
 //
 //        FragmentManager fragmentManager = getFragmentManager();
 //        fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
 //
 //        // update selected item and title, then close the drawer
-//        mDrawerList.setItemChecked(position, true);
-//        setTitle(mPlanetTitles[position]);
-//        mDrawerLayout.closeDrawer(mDrawerList);
+//        drawerList.setItemChecked(position, true);
+//        setTitle(fragmentTitles[position]);
+//        drawerLayout.closeDrawer(drawerList);
 //    }
 //
 //    @Override
 //    public void setTitle(CharSequence title) {
-//        mTitle = title;
-//        getActionBar().setTitle(mTitle);
+//        title = title;
+//        getActionBar().setTitle(title);
 //    }
 //
 //    /**
@@ -173,31 +215,31 @@ public class MainActivity extends Activity {
 //    protected void onPostCreate(Bundle savedInstanceState) {
 //        super.onPostCreate(savedInstanceState);
 //        // Sync the toggle state after onRestoreInstanceState has occurred.
-//        mDrawerToggle.syncState();
+//        drawerToggle.syncState();
 //    }
 //
 //    @Override
 //    public void onConfigurationChanged(Configuration newConfig) {
 //        super.onConfigurationChanged(newConfig);
 //        // Pass any configuration change to the drawer toggls
-//        mDrawerToggle.onConfigurationChanged(newConfig);
+//        drawerToggle.onConfigurationChanged(newConfig);
 //    }
 //
 //    /**
 //     * Fragment that appears in the "content_frame", shows a planet
 //     */
-//    public static class PlanetFragment extends Fragment {
-//        public static final String ARG_PLANET_NUMBER = "planet_number";
+//    public static class EmptyFragment extends Fragment {
+//        public static final String ARG_FRAGMENT_NUMBER = "planet_number";
 //
-//        public PlanetFragment() {
+//        public EmptyFragment() {
 //            // Empty constructor required for fragment subclasses
 //        }
 //
 //        @Override
 //        public View onCreateView(LayoutInflater inflater, ViewGroup container,
 //                                 Bundle savedInstanceState) {
-//            View rootView = inflater.inflate(R.layout.fragment_planet, container, false);
-//            int i = getArguments().getInt(ARG_PLANET_NUMBER);
+//            View rootView = inflater.inflate(R.layout.fragment_empty, container, false);
+//            int i = getArguments().getInt(ARG_FRAGMENT_NUMBER);
 //            String planet = getResources().getStringArray(R.array.planets_array)[i];
 //
 //            int imageId = getResources().getIdentifier(planet.toLowerCase(Locale.getDefault()),
